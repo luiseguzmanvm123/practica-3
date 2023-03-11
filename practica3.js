1af345c0dcdb4cab623aaf6bcb13f9c5048bc2e3
Luis Enrique <127609212+luiseguzmanvm123@users.noreply.github.com>
2023-03-11 09:01:28 -0600

    Add files via upload

---
+++ practica3.js
@@ -0,0 +1,32 @@
+/* let nombre=prompt("¿cual es tu nombre?"); //
+console.log(nombre);
+let numero=prompt("¿cual es tu edad?");
+console.log(numero);
+let x=2;
+const y=3;
+console.log(x);
+console.log(x=y+1);
+console.log(x=x*y);
+
+let nu1=prompt(1);
+let nu2=prompt(2);
+let suma=(parseInt(nu1))+(parseInt(nu2));
+console.log(suma);
+console.log(nu1*nu2);
+console.log(nu1/nu2) */
+
+let nom1 = prompt("¿como te llamas?");
+alert("hola " + nom1);
+let num1 = prompt("hola " + nom1 + " dame tu numero");
+let num2=prompt("hola " + nom1 + " dame tu segundo numero");
+let suma=(parseInt(num1))+(parseInt(num2));
+
+console.log(num1);
+
+
+console.log(suma);
+console.log(num1*num2);
+console.log(num1/num2);
+console.log(num1-num2);
+
+
